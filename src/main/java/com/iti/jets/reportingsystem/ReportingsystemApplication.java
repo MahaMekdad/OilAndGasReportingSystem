6abeba39999a20ddf503600bbf9139798b6eb5ca
MahaMekdad <maha_mekdad@outlook.com>
2021-06-04 21:32:06 +0200

    init repo

---
+++ src/main/java/com/iti/jets/reportingsystem/ReportingsystemApplication.java
@@ -0,0 +1,13 @@
+package com.iti.jets.reportingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iti/jets/reportingsystem/ReportingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iti.jets.reportingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
