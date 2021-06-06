e8dde284d70f83c36c5acfbef1d7a2f99952c0f8
Salma Elkady <salmaelkady1997@gmail.com>
2021-06-06 16:56:44 +0200

    merge dev into drilling info branch

---
+++ src/main/java/com/iti/jets/reportingsystem/repos/DrillingInfoRepo.java
@@ -0,0 +1,7 @@
+package com.iti.jets.reportingsystem.repos;
+
+import com.iti.jets.reportingsystem.entities.DrillingInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DrillingInfoRepo extends JpaRepository<DrillingInfo, String > {
+}
