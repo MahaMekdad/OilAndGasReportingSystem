3dc6e1754639c80fecbd9e7a15b7bc7026b78336
Salma Elkady <salmaelkady1997@gmail.com>
2021-06-07 22:03:24 +0200

    driiling info issue

--- src/main/java/com/iti/jets/reportingsystem/services/impls/DrillingServiceImpl.java
+++ src/main/java/com/iti/jets/reportingsystem/services/impls/DrillingServiceImpl.java
@@ -1,12 +1,15 @@
 package com.iti.jets.reportingsystem.services.impls;
 
 import com.iti.jets.reportingsystem.entities.DrillingInfo;
+import com.iti.jets.reportingsystem.entities.ProductionBudget;
 import com.iti.jets.reportingsystem.models.DrillingInfoModel;
 import com.iti.jets.reportingsystem.repos.DrillingInfoRepository;
 import com.iti.jets.reportingsystem.services.DrillingInfoService;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 @Service
@@ -18,8 +21,18 @@ public class DrillingServiceImpl implements DrillingInfoService {
 
         List<DrillingInfoModel> drillingInfoModelList = new ArrayList<>();
         List<DrillingInfo> drillingInfooo = drillingInfoRepository.findAll();
+        Type listType = new TypeToken<List<DrillingInfoModel>>(){}.getType();
+        drillingInfoModelList=
 
-//        drillingInfoModelList =
+//
+//        List<ProductionBudget> productionBudget = new ArrayList<>();
+//        productionBudget = productionBudgetRepository.findAll();
+//        System.out.println("productionBudgetRepository.findAll()============ " + productionBudget);
+//        List<ProductionBudgetModel> productionBudgetModels = new ArrayList<>();
+//        Type listType = new TypeToken<List<ProductionBudgetModel>>(){}.getType();
+//        productionBudgetModels = modelMapper.map(productionBudget,listType);
+//        System.out.println("Production budgetData================ "+productionBudgetModels);
+//        return productionBudgetModels;
         return drillingInfoModelList;
     }
 
