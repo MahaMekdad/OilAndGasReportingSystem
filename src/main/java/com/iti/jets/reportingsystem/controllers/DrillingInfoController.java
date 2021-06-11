44c81ef79ae8f741e30573be18bb56f06b1c0b84
Salma Elkady <salmaelkady1997@gmail.com>
2021-06-11 22:40:42 +0200

    done with date issue

--- src/main/java/com/iti/jets/reportingsystem/controllers/DrillingInfoController.java
+++ src/main/java/com/iti/jets/reportingsystem/controllers/DrillingInfoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+
 @RestController
 public class DrillingInfoController implements WellsApi {
     @Autowired
@@ -18,40 +19,61 @@ public class DrillingInfoController implements WellsApi {
     @Override
     public ResponseEntity<Void> wellsWellIdDrillingInfoDelete(Integer wellId) {
         drillingInfoService.delete(wellId);
+        return ResponseEntity.ok().build();
     }
 
     @Override
     public ResponseEntity<List<DrillingInfoDataResponse>> wellsWellIdDrillingInfoGet(Integer wellId) {
         List<DrillingInfoDataResponse> drillingInfoDataResponses = drillingInfoService.getForWellId(wellId);
-        return drillingInfoDataResponses;
+        if (drillingInfoDataResponses == null)
+            return ResponseEntity.notFound().build();
+        else {
+            return ResponseEntity.ok(drillingInfoDataResponses);
+        }
     }
 
     @Override
     public ResponseEntity<Void> wellsWellIdDrillingInfoIdDelete(Integer wellId, Integer id) {
         drillingInfoService.deleteWellInSpecificId(wellId , id);
+        return ResponseEntity.ok().build();
     }
 
     @Override
-    public ResponseEntity<List<DrillingInfoDataResponse>> wellsWellIdDrillingInfoIdGet(Integer wellId, Integer id) {
-         drillingInfoService.getWellForId(wellId , id);
+    public ResponseEntity<DrillingInfoDataResponse> wellsWellIdDrillingInfoIdGet(Integer wellId, Integer id) {
+        DrillingInfoDataResponse drillingInfoDataResponse =drillingInfoService.getWellForId(wellId , id);
+        if(drillingInfoDataResponse == null)
+        {
+            return ResponseEntity.notFound().build();
+        }
+        else {
+            return ResponseEntity.ok(drillingInfoDataResponse);
+        }
     }
 
     @Override
     public ResponseEntity<Void> wellsWellIdDrillingInfoIdPatch(Integer wellId, Integer id, DrillingInfoDataRequest drillingInfoDataRequest) {
         drillingInfoService.updateWellForId(wellId ,id ,drillingInfoDataRequest);
+        return ResponseEntity.ok().build();
     }
 
     @Override
     public ResponseEntity<Void> wellsWellIdDrillingInfoPost(Integer wellId, DrillingInfoDataRequest drillingInfoDataRequest) {
-        drillingInfoService.creat(drillingInfoDataRequest , wellId);
-        return ResponseEntity.ok();
+        drillingInfoService.creat(drillingInfoDataRequest, wellId);
+        return ResponseEntity.ok().build();
+
     }
 
     @Override
     public ResponseEntity<List<DrillingInfoDataResponse>> wellsDrillingInfoGet() {
         List<DrillingInfoDataResponse> drillingInfoDataResponses = new ArrayList<>();
         drillingInfoDataResponses = drillingInfoService.getAllDrillingInfo();
-        return drillingInfoDataResponses;
+        if (drillingInfoDataResponses == null)
+            return ResponseEntity.notFound().build();
+        else {
+            return ResponseEntity.ok(drillingInfoDataResponses);
+        }
+
     }
+
 
 }
